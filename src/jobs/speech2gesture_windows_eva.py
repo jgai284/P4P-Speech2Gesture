35e6ee2118ad48fc80ff450da32d642a82e1a572
jgai284 <jgai284@aucklanduni.ac.nz>
2024-06-14 14:51:32 +1200

    updated scripts and ignore

---
+++ src/jobs/speech2gesture_windows_eva.py
@@ -0,0 +1,1 @@
+python sample.py -load "D:\UoA\SOFTENG 700A\P4P-Speech2Gesture\save\speech2gesture\oliver\exp_105_cpk_speech2gesture_speaker_['oliver']_model_Speech2Gesture_G_note_speech2gesture_weights.p" -path2data "F:\PATSDATASET\oliver\pats\data"
